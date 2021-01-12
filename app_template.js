62da41c63990dd36bd76ed50345742193bbe507c
ultramanbabe <ultraman_babeclub@hotmail.com>
2021-01-12 13:58:20 +0700

    3

--- app_template.js
+++ app_template.js
@@ -1,5 +1,5 @@
 //your code here!
 function myFunction() {
     var x = document.getElementById("input-number").value;
-    document.getElementById("input-number").innerHTML = 620612157 - input-number;
+    document.getElementById("button is-primary").innerHTML = 620612157 - input-number;
 }
